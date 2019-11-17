cc6ef38105bcc94bec130c36ac121dfafd51dd2d
Ahmed Saad <ahmed.saad200253@gmail.com>
2019-11-17 06:09:42 +0200

    Created on Boarding Screen

---
+++ lib/onBoardingScreen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  final pages = [
+    PageViewModel(
+      pageColor: Colors.indigo,
+      bubbleBackgroundColor: Colors.white,
+      title: Container(),
+      body: Column(
+        children: <Widget>[
+          Text('Medical information', style: TextStyle(fontWeight: FontWeight.w900, color: Colors.white),),
+          Text(
+            'Introduce your medical information, anytime, anywhere.',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+      mainImage: Image.asset(
+        'assets/service1.png',
+        width: 370.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(color: Colors.black),
+    ),
+    PageViewModel(
+      pageColor: Colors.redAccent,
+      iconColor: null,
+      bubbleBackgroundColor: Colors.white,
+      title: Container(),
+      body: Column(
+        children: <Widget>[
+          Text('Uniqe Rescue code', style: TextStyle(fontWeight: FontWeight.w900, color: Colors.white)),
+          Text(
+            'Have your own personal rescue code for emergencies',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0
+            ),
+          ),
+        ],
+      ),
+      mainImage: Image.asset(
+        'assets/service2.png',
+        width: 370,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(color: Colors.black),
+    ),
+    PageViewModel(
+      pageColor: Colors.green,
+      iconColor: null,
+      bubbleBackgroundColor: Colors.white,
+      title: Container(),
+      body: Column(
+        children: <Widget>[
+          Text('Report Accidents', style: TextStyle(fontWeight: FontWeight.w900, color: Colors.white)),
+          Text(
+            'Report Accidents and send your live location to the hospital.',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0
+            ),
+          ),
+        ],
+      ),
+      mainImage: Image.asset(
+        'assets/service3.png',
+        width: 370.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(color: Colors.black),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            IntroViewsFlutter(
+              pages,
+              onTapDoneButton: (){
+                Navigator.pushNamed(context, "landingpage");
+              },
+              showSkipButton: false,
+              doneText: Text("Get Started",),
+              pageButtonsColor: Colors.white,
+              pageButtonTextStyles: new TextStyle(
+                // color: Colors.indigo,
+                fontSize: 16.0,
+                fontFamily: "Regular",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
