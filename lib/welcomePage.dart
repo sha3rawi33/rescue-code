8b2e85076b5d9e0224024a2337d71c8d84650f9b
Ahmed Saad <ahmed.saad200253@gmail.com>
2019-11-30 05:26:54 +0200

    Revert "Revert "Fix Syntax Problems""
    This reverts commit 7274ba0ce29eecdb4c009c72cc8f900c68822a54.

--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rescue_code/profilePage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'doctorChat.dart';
 import 'style/delayedAnimation.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:rescue_code/style/theme.dart' as Theme;
@@ -8,7 +11,8 @@ class WelcomePage extends StatefulWidget {
   _WelcomePageState createState() => _WelcomePageState();
 }
 
-class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStateMixin {
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
   final int delayedAmount = 500;
   double _scale;
   AnimationController _controller;
@@ -32,135 +36,155 @@ class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStat
   Widget build(BuildContext context) {
     final color = Colors.white;
     _scale = 1 - _controller.value;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height >= 775.0
-            ? MediaQuery.of(context).size.height
-            : 775.0,
-        decoration: new BoxDecoration(
-          gradient: new LinearGradient(
-              colors: [
-                Theme.Colors.loginGradientStart,
-                Theme.Colors.loginGradientEnd
-              ],
-              begin: const FractionalOffset(0.0, 0.0),
-              end: const FractionalOffset(1.0, 1.0),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.clamp),
-        ),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(width: 20,height: 100,),
-              AvatarGlow(
-                endRadius: 90,
-                duration: Duration(seconds: 2),
-                glowColor: Colors.white24,
-                repeat: true,
-                repeatPauseDuration: Duration(seconds: 2),
-                startDelay: Duration(seconds: 1),
-                child: Material(
-                    elevation: 8.0,
-                    shape: CircleBorder(),
-                    child: CircleAvatar(
-                      backgroundColor: Colors.grey[100],
-                      child: Icon(
-                        Icons.healing,
-                        size: 50,
-                        color: Colors.redAccent,
-                      ),
-                      radius: 50.0,
-                    )),
-              ),
-              DelayedAnimation(
-                child: Text(
-                  "Hi There",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35.0,
-                      color: color),
-                ),
-                delay: delayedAmount + 1000,
-              ),
-              DelayedAnimation(
-                child: Text(
-                  "I'm Rescue Code",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35.0,
-                      color: color),
-                ),
-                delay: delayedAmount + 2000,
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              DelayedAnimation(
-                child: Text(
-                  "I aim to scrimp time",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: color,
-                  ),
-                ),
-                delay: delayedAmount + 3000,
-              ),
-              DelayedAnimation(
-                child: Text(
-                  "needed for rescuing humans",
-                  style: TextStyle(fontSize: 20.0, color: color),
-                ),
-                delay: delayedAmount + 3000,
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              DelayedAnimation(
-                child: GestureDetector(
-                  onTapDown: _onTapDown,
-                  onTapUp: _onTapUp,
-                  child: Transform.scale(
-                    scale: _scale,
-                    child: _animatedButtonUI,
-                  ),
-                ),
-                delay: delayedAmount + 4000,
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              DelayedAnimation(
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "PLEBITS LLC 2019",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14.0,
-                            fontFamily: "customFont",
-                            color: color),
-                      ),
-                      Icon(Icons.copyright, color: Colors.white, size: 20,)
-                    ],
-                  ),
-                ),
-                delay: delayedAmount + 1000,
-              ),
+
+    return Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height >= 775.0
+          ? MediaQuery.of(context).size.height
+          : 775.0,
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+            colors: [
+              Theme.Colors.loginGradientStart,
+              Theme.Colors.loginGradientEnd
             ],
-          ),
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: 20,
+              height: 100,
+            ),
+            AvatarGlow(
+              endRadius: 90,
+              duration: Duration(seconds: 2),
+              glowColor: Colors.white24,
+              repeat: true,
+              repeatPauseDuration: Duration(seconds: 2),
+              startDelay: Duration(seconds: 1),
+              child: Material(
+                  elevation: 8.0,
+                  shape: CircleBorder(),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey[100],
+                    child: Icon(
+                      Icons.healing,
+                      size: 50,
+                      color: Colors.redAccent,
+                    ),
+                    radius: 50.0,
+                  )),
+            ),
+            DelayedAnimation(
+              child: Text(
+                "Hi There",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 35.0, color: color),
+              ),
+              delay: delayedAmount + 1000,
+            ),
+            DelayedAnimation(
+              child: Text(
+                "I'm Rescue Code",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 35.0, color: color),
+              ),
+              delay: delayedAmount + 2000,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            DelayedAnimation(
+              child: Text(
+                "I aim to scrimp time",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: color,
+                ),
+              ),
+              delay: delayedAmount + 3000,
+            ),
+            DelayedAnimation(
+              child: Text(
+                "needed for rescuing humans",
+                style: TextStyle(fontSize: 20.0, color: color),
+              ),
+              delay: delayedAmount + 3000,
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            DelayedAnimation(
+              child: GestureDetector(
+                onTapDown: _onTapDown,
+                onTapUp: _onTapUp,
+                child: Transform.scale(
+                  scale: _scale,
+                  child: _animatedButtonUI,
+                ),
+              ),
+              delay: delayedAmount + 4000,
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            DelayedAnimation(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "PLEBITS LLC 2019",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.0,
+                          fontFamily: "customFont",
+                          color: color),
+                    ),
+                    Icon(
+                      Icons.copyright,
+                      color: Colors.white,
+                      size: 20,
+                    )
+                  ],
+                ),
+              ),
+              delay: delayedAmount + 1000,
+            ),
+          ],
         ),
-      )),
-    );
+      ),
+    ));
   }
 
   Widget get _animatedButtonUI => GestureDetector(
-    onTap: () => Navigator.pushNamed(context, "boarding"),
-    child: Container(
+        onTap: () async {
+          SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+          var uid = _prefs.getString("uid");
+          var type = _prefs.getString("type");
+
+          if (uid != null && uid != '') {
+            if (type == "doctor") {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => DoctorChat()));
+            } else {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ProfilePage(uid: uid)));
+            }
+          } else {
+            Navigator.pushNamed(context, "boarding");
+          }
+        },
+        child: Container(
           height: 60,
           width: 270,
           decoration: BoxDecoration(
@@ -178,8 +202,7 @@ class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStat
             ),
           ),
         ),
-  );
-
+      );
 
   void _onTapDown(TapDownDetails details) {
     _controller.forward();
